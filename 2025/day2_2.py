6d225d46ade3101b5333c7ec7a56da1131d38c38
Caroline <treu.caroline@gmail.com>
2025-12-04 22:50:40 +0200

    day2_2

--- 2025/day2_2.py
+++ 2025/day2_2.py
@@ -1,3 +1,5 @@
+import re
+
 funnyNums=[]
 
 with open("2025/inputs/input2.txt") as file_in:
@@ -7,8 +9,8 @@ for area in output:
     ranges = area.split('-')
 
     for val in range(int(ranges[0]), int(ranges[1])):
-        currentNum=str(val)
-        if currentNum[:len(currentNum)//2] == currentNum[len(currentNum)//2:]:
+        pos = re.search(r"^(.+)(\1+)$", str(val))
+        if pos is not None:
             funnyNums.append(val)
 
 print(sum(funnyNums))
